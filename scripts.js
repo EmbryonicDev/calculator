efe90f10d64721ad7f40124da43c4e39d7054bb4
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-20 10:11:23 +0100

    \+ operate function & test

--- scripts.js
+++ scripts.js
@@ -18,16 +18,38 @@ function devide(a, b) {
   return answer;
 }
 
+function operate(operator, num1, num2) {
+  let answer =""
+  if(operator == "+") {
+    answer = add(num1, num2);
+    return answer;
+  } else if(operator == "-") {
+    answer = subtract(num1, num2);
+    return answer;
+  } else if(operator == "*") {
+    answer = multiply(num1, num2);
+    return answer;
+  } else if(operator == "/") {
+    answer = devide(num1, num2);
+    return answer;
+  }  
+}
+
+console.log(operate('+', 5, 5));
+console.log(operate("-", 20, 5));
+console.log(operate("*", 20, 5));
+console.log(operate("/", 20, 5));
+
 // ------------------------
-console.log("add");
-console.log(add(9,9));
+// console.log("add");
+// console.log(add(9,9));
 
-console.log("subtract");
-console.log(subtract(19,9));
+// console.log("subtract");
+// console.log(subtract(19,9));
 
-console.log("multiply");
-console.log(multiply(9,9));
+// console.log("multiply");
+// console.log(multiply(9,9));
 
-console.log("devide");
-console.log(devide(27, 3));
+// console.log("devide");
+// console.log(devide(27, 3));
 // --------------------------
