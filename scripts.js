6b43d7409070def88ee70a4b269cbd4a85de6178
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-20 10:10:29 +0100

    \+ functions to "+, -, *, /" & test

---
+++ scripts.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  let answer = a + b;
+  return answer;
+}
+
+function subtract(a, b) {
+  let answer = a - b;
+  return answer;
+}
+
+function multiply(a, b) {
+  let answer = a * b;
+  return answer;
+}
+
+function devide(a, b) {
+  let answer = a / b;
+  return answer;
+}
+
+// ------------------------
+console.log("add");
+console.log(add(9,9));
+
+console.log("subtract");
+console.log(subtract(19,9));
+
+console.log("multiply");
+console.log(multiply(9,9));
+
+console.log("devide");
+console.log(devide(27, 3));
+// --------------------------
